d2e5c659ef0a1a3f00d5bc3152a59e6324e4bb63
Aliaksandr Melnichenka <melnichenkaa@berea.edu>
2024-07-06 14:04:34 +0300

    test

--- _plugins/centered_figure.rb
+++ _plugins/centered_figure.rb
@@ -1,15 +1,19 @@
 module Jekyll
-  class FigureTag < Liquid::Tag
+  class CenteredImageTag < Liquid::Tag
     def initialize(tag_name, markup, tokens)
       super
-      @alt = markup.strip
-      @url = markup.strip.split('](').last.split(')').first
+      @url, @alt = markup.strip.split(/\s+/, 2)
     end
 
     def render(context)
-      "<center><figure><img src='#{@url}' loading='lazy' alt='#{@alt}' width='500' /><figcaption>#{@alt}</figcaption></figure></center>"
+      "<center>
+        <figure>
+          <img src=\"#{@url}\" loading=\"lazy\" alt=\"#{@alt}\" width=\"500\" />
+          <figcaption>#{@alt}</figcaption>
+        </figure>
+      </center>"
     end
   end
 end
 
-Liquid::Template.register_tag('figure', Jekyll::FigureTag)
+Liquid::Template.register_tag('centered_image', Jekyll::CenteredImageTag)
